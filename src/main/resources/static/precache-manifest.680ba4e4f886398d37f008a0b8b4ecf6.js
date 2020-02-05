c75bae3bc363568bd46c58d104ae197cc4e62eb8
priyo153 <priyojit.chatterjee333@gmail.com>
2020-02-05 10:10:35 +0530

    updated static folder with build contents of front end application

---
+++ src/main/resources/static/precache-manifest.680ba4e4f886398d37f008a0b8b4ecf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4c34eca015eed613bde935b5c42c79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "263e21a778f4380c532a",
+    "url": "/static/css/main.e0de7674.chunk.css"
+  },
+  {
+    "revision": "9fcc0caa4de9dfe3b145",
+    "url": "/static/js/2.2f44c0b4.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.2f44c0b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "263e21a778f4380c532a",
+    "url": "/static/js/main.c2126dcc.chunk.js"
+  },
+  {
+    "revision": "e8dfed90e7b305c7bb0a",
+    "url": "/static/js/runtime-main.28411650.js"
+  }
+]);
